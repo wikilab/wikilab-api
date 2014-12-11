ad4f2512ab1e42bf19b877c04eddd831cf747fb0
Zihua Li <i@zihua.li>
2014-12-11 14:20:59 +0800

    Remove unnecessary instance methods of collection

--- models/collections.js
+++ models/collections.js
@@ -6,24 +6,5 @@ module.exports = function(DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     }
-  }, {
-    instanceMethods: {
-      insertIntoDirs: function(UUID, parentUUID) {
-        var tree = new TreeModel();
-        var dirs = tree.parse(JSON.parse(this.dirs));
-        var parent;
-        if (parentUUID) {
-          parent = dirs.first(function(node) {
-            return node.model.UUID === parent;
-          });
-        }
-        if (!parent) {
-          parent = dirs;
-        }
-        parent.addChild(tree.parse({ UUID: UUID }));
-        this.dirs = JSON.stringify(dirs.model);
-        return this.save(['dirs']);
-      }
-    }
   }];
 };
