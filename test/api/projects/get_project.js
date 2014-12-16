9732c1756177213c422f26ce28cb26a589c5c6c5
Zihua Li <i@zihua.li>
2014-12-16 16:50:09 +0800

    Finish the pending tests

--- test/api/projects/get_project.js
+++ test/api/projects/get_project.js
@@ -36,14 +36,12 @@ describe('GET /projects/:projectId', function() {
     }
   });
 
-  it('should return project and it\'s collections'/*, function *() {
+  it('should return project and it\'s collections', function *() {
     var user = fixtures.users[0];
     var project = fixtures.projects[0];
-    return api.$auth(user.email, user.password).projects(project.id).get().then(function(returnedProject) {
-      expect(returnedProject).to.have.property('id', project.id);
-      expect(returnedProject).to.have.property('name', project.name);
-      expect(returnedProject.collections).to.have.length(2);
-    }).catch(function(err) {
-      console.log(err.statusCode);
-    }*/);
+    var returnedProject = yield api.$auth(user.email, user.password).projects(project.id).get();
+    expect(returnedProject).to.have.property('id', project.id);
+    expect(returnedProject).to.have.property('name', project.name);
+    expect(returnedProject.collections).to.have.length(2);
+  });
 });
